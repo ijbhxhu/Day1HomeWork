cb746ec7ed0feb54a930ec940ce8bfad640caa0b
Eric <Eric@ERIC-NB>
2016-08-05 21:21:16 +0800

    加入專案檔案。

---
+++ Day1HomeWork/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Day1HomeWork.Startup))]
+namespace Day1HomeWork
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
